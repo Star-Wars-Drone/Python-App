ef480b1f3bd53d7efff9135177cb613ff6919dc5
Ahmed Samara <asamara@ncsu.edu>
2017-04-10 20:30:17 -0400

    perfect color filtering, but issues with shape being recognized as ellipse

--- image-processing/balloon_detector.py
+++ image-processing/balloon_detector.py
@@ -15,16 +15,22 @@ detector = cv2.SimpleBlobDetector()
 
 # hsv red is [[[  0 255 255]]]
 low_red = np.array([0, 100, 100])
-upper_red = np.array([20, 255, 255])
+upper_red = np.array([255, 255, 255])
 
+
+#low_red = np.array([300, 70, 70])
+#upper_red = np.array([360, 255, 255])
 
 def filters(frame):
     # gray and blur
-    frame = cv2.GaussianBlur(frame,(0,0),3)
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    #frame = cv2.GaussianBlur(frame,(0,0),3)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    mask = cv2.inRange(frame, low_red, upper_red)
+    mask = cv2.inRange(hsv, low_red, upper_red)
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
 
+    cv2.imshow('mask', mask)
     frame = cv2.bitwise_and(frame,frame,mask=mask)
     
     #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -33,6 +39,21 @@ def filters(frame):
 
 
     return frame
+
+
+def hsv_range(frame):
+    """tells lowest and highest h-s-v values of an obj"""
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    height, width, d = frame.shape
+
+    hsv_lower = hsv[0,0]
+    hsv_upper = [0, 0, 0]
+ 
+    for i in range(0, height):
+        for j in range(0, width):
+            i=0        
+
 
 
 def redout(frame):
@@ -68,29 +89,47 @@ def is_round(contour):
 
     # expect very large # of edges
     if len(approx) > 5:
-        print "found round"
         return True
     return False    
+
+font = cv2.FONT_HERSHEY_SIMPLEX
 
 def is_elliptical(contour):
     """git an ellipse, check if that ellipse is still close"""
 
-    ell = cv2.fitEllipse(contour)
+    if len(contour) < 5:
+        return False
+    (x,y), (MA,ma), angle = cv2.fitEllipse(contour)
+    ellipse = cv2.fitEllipse(contour)
+
+    ell_area = 3.14*(MA/2)*(ma/2)
+
     eps = 0.1*cv2.arcLength(contour,True)
 
     aprx = cv2.approxPolyDP(contour, eps, True)
+    area = cv2.contourArea(aprx)
 
-    ratio = cv2.contourArea(ell)/cv2.contourArea(aprx)  
-    return 0.8 < ratio < 1.2
+    string = "ell: {0:0.2f}, area: {0:0.2f}".format(ell_area, area)
+
+    cv2.ellipse(im, ellipse, (0,0,255),2)
+    cv2.putText(im, string, (int(x),int(y)), font, 1, (0,255,0), 2)
+    #cv2.putText(im,'OpenCV',(10,500), font, 4,(255,255,255),2)
+
+
+    if area > 0.0:  
+        ratio = ell_area/area
+
+        return 0.9 < ratio < 1.1
+    return False
 
 
 def is_balloon(contour):
     
-    sld = is_solid(contour)
-    rnd = is_round(contour)
-    #ep = is_elliptical(contour)
-
-    return sld and rnd #and ep
+    #sld = is_solid(contour)
+    #rnd = is_round(contour)
+    ep = is_elliptical(contour)
+    
+    return ep
 
 
 
@@ -103,19 +142,34 @@ while True:
     # im_with_blobs = cv2.drawKeypoints(im, blob_points, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
     #cv2.imshow('points', im_with_blobs)
     cv2.imshow('none', im)
-
-    #frame = filters(im)
-    i=0
-    rframe = redout(im)
-
-    cv2.imshow('red', rframe)
-    #cv2.imshow('filtered', frame)
     
-    #contours, hierarchy = cv2.findContours(frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    hsv = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
 
-    #cv2.drawContours(im, contours, -1, (0,255,0), 3)
+    mask = cv2.inRange(hsv, low_red, upper_red)
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
 
-    # TODO(AHmed) Consider using contour approx before doing this to 'close' shapes
+
+    cv2.imshow('mask', mask)
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+
+
+    cv2.drawContours(im, cnts, -1, (0,255,0), 3)
+
+    for c in cnts:
+        #peri = cv2.arcLength(c, True)
+        #approx = cv2.approxPolyDP(c, 0.02*peri, True)
+        if is_balloon(c):
+            cv2.drawContours(im, [c], 0, (255,0,0), 8)
+    #frame = cv2.bitwise_and(frame,frame,mask=mask)
+    #gray = cv2.cvtColor(frame, cv2.COLOR_HSV2GRAY)
+   
+    #ret, thresh = cv2.threshold(gray, 127, 255, 0)
+
+
+    cv2.imshow('filtered', im)
+
+    # TODO(Ahmed) Consider using contour approx before doing this to 'close' shapes
     # This may ruin everything actually.
 
     #for c in contours:
