b6c7d592a923567ee0091b23962b853e5894c87d
hemanth2406 <hemanth2406@gmail.com>
2017-04-14 16:35:40 -0400

    indentation errors

--- image-processing/ball_tracking.py
+++ image-processing/ball_tracking.py
@@ -147,169 +147,168 @@ class BalloonFinder(object):
 		return int(angle * self.img_width / math.radians(self.cam_hfov))
 
 	def project_position(self, origin, pitch, yaw, distance):
-        cos_pitch = math.cos(pitch)
-        dx = distance * math.cos(yaw) * cos_pitch
-        dy = distance * math.sin(yaw) * cos_pitch
-        dz = distance * math.sin(pitch)
-        ret = PositionVector(origin.x + dx,origin.y + dy, origin.z + dz)
-        return ret
+		cos_pitch = math.cos(pitch)
+		dx = distance * math.cos(yaw) * cos_pitch
+		dy = distance * math.sin(yaw) * cos_pitch
+		dz = distance * math.sin(pitch)
+		ret = PositionVector(origin.x + dx,origin.y + dy, origin.z + dz)
+		return ret
 
 	def __init__(self):
-        self.cam = cv2.VideoCapture(0)
-        self.im_cnt =0;
-        self.vid_cnt = 0;
-        self.low_red = np.array([0, 100, 100])
-        self.upper_red = np.array([255, 255, 255])
+		self.cam = cv2.VideoCapture(0)
+		self.im_cnt =0;
+		self.vid_cnt = 0;
+		self.low_red = np.array([0, 100, 100])
+		self.upper_red = np.array([255, 255, 255])
 
 	def save_image(self):
-        ret, im = self.cam.read()
-        filename = 'IM_CAP_' + str(self.im_cnt) + '.jpg'
-        self.im_cnt = self.im_cnt + 1
-        cv2.imwrite(filename, im)
-        
+		ret, im = self.cam.read()
+		filename = 'IM_CAP_' + str(self.im_cnt) + '.jpg'
+		self.im_cnt = self.im_cnt + 1
+		cv2.imwrite(filename, im)
+                
 	def save_video(self, length=3):
-        """saves a 1 min video by default"""
-        #thread.start()
-        t0 = time.time()
-        filename = 'VID_CAP_' + str(self.vid_cnt) + '.avi'
+		"""saves a 1 min video by default"""
+		#thread.start()
+		t0 = time.time()
+		filename = 'VID_CAP_' + str(self.vid_cnt) + '.avi'
+		# test frame
+		ret, frame = self.cam.read()     
+		height, width, ch = frame.shape  
 
-        # test frame
-        ret, frame = self.cam.read()     
-        height, width, ch = frame.shape  
-
-        self.vid_cnt = self.vid_cnt+1
-        fourcc = cv2.cv.CV_FOURCC(*'XVID')
-        vid_writer = cv2.VideoWriter(filename,fourcc, 20, (width,height)) 
-        while (time.time() - length) < t0:
-            ret, frame = self.cam.read()
-            vid_writer.write(frame)
-        vid_writer.release()
-
-
-
-    def is_solid(self, contour):
-        """ Checks to see if the contour is "solid".
-        hard to describe in text but google convexHull"""
-
-        area = cv2.contourArea(contour)
-        hullArea = cv2.contourArea(cv2.convexHull(contour))
-        if hullArea > 0:
-            solidity = area / float(hullArea)
-        else:
-            return False
-
-        solid = solidity  > 0.9
-        return solid
-
-
-    def is_round(self, contour):
-        """checks that contour is many edged"""
-
-        peri = cv2.arcLength(contour, True)
-        approx = cv2.approxPolyDP(contour, 0.02*peri, True)
-
-        # expect very large # of edges
-        if len(approx) > 5:
-            return True
-        return False    
-
-    def is_elliptical(self, contour):
-        """git an ellipse, check if that ellipse is still close"""
-
-        if len(contour) < 5:
-            return False
-        (x,y), (MA,ma), angle = cv2.fitEllipse(contour)
-        ellipse = cv2.fitEllipse(contour)
-
-        ell_area = 3.14159264*(MA/2)*(ma/2)
-
-        eps = 0.1*cv2.arcLength(contour,True)
-        aprx = cv2.approxPolyDP(contour, eps, True)
-        area = cv2.contourArea(aprx)
-
-        if area > 300:  
-            ratio = float(ell_area)/float(area)
-            string = "area: " + str(area) + "ell_area: " + str(ell_area)
-            #cv2.putText(im, string, (int(x),int(y)), font, 1, (0,255,0), 1)     
-            #cv2.line(im, (int(x), int(y)), (int(x),int(y)+int(MA/2)), (255,0,255))
-            #cv2.line(im, (int(x), int(y)), (int(x)-int(ma/2),int(y)),(255,0,255))
-        else:
-            return False
-
-        #string = "ell: {0:0.2f}, area: {0:0.2f},\n ratio: {0:0.2f}".format(ell_area, area, ratio)
-        #string = "ratio: {0:0.2f}".format(ratio)
-        #cv2.ellipse(im, ellipse, (0,0,255),2)
-        #cv2.putText(im, string, (int(x),int(y)), font, 1, (0,255,0), 1)
-        #cv2.putText(im,'OpenCV',(10,500), font, 4,(255,255,255),2)
+		self.vid_cnt = self.vid_cnt+1
+		fourcc = cv2.cv.CV_FOURCC(*'XVID')
+		vid_writer = cv2.VideoWriter(filename,fourcc, 20, (width,height)) 
+		while (time.time() - length) < t0:
+			ret, frame = self.cam.read()
+			vid_writer.write(frame)
+		vid_writer.release()
 
 
 
-        if 1.4 < ratio < 1.6:
-            #cv2.putText(im, "Balloon", (int(x),int(y)), font, 1, (255,0,255), 9)
-            return True
-        else:
-            return False
+	def is_solid(self, contour):
+		""" Checks to see if the contour is "solid".
+		hard to describe in text but google convexHull"""
+
+		area = cv2.contourArea(contour)
+		hullArea = cv2.contourArea(cv2.convexHull(contour))
+		if hullArea > 0:
+			solidity = area / float(hullArea)
+		else:
+			return False
+
+		solid = solidity  > 0.9
+		return solid
 
 
-    def is_balloon(self, contour):
-        
-        sld = self.is_solid(contour)
-        rnd = self.is_round(contour)
-        ep = self.is_elliptical(contour)
-        
-        return (ep and sld and rnd)
+	def is_round(self, contour):
+		"""checks that contour is many edged"""
+
+		peri = cv2.arcLength(contour, True)
+		approx = cv2.approxPolyDP(contour, 0.02*peri, True)
+
+		# expect very large # of edges
+		if len(approx) > 5:
+			return True
+		return False    
+
+	def is_elliptical(self, contour):
+		"""git an ellipse, check if that ellipse is still close"""
+
+		if len(contour) < 5:
+			return False
+		(x,y), (MA,ma), angle = cv2.fitEllipse(contour)
+		ellipse = cv2.fitEllipse(contour)
+
+		ell_area = 3.14159264*(MA/2)*(ma/2)
+
+		eps = 0.1*cv2.arcLength(contour,True)
+		aprx = cv2.approxPolyDP(contour, eps, True)
+		area = cv2.contourArea(aprx)
+
+		if area > 300:  
+			ratio = float(ell_area)/float(area)
+			string = "area: " + str(area) + "ell_area: " + str(ell_area)
+			#cv2.putText(im, string, (int(x),int(y)), font, 1, (0,255,0), 1)     
+			#cv2.line(im, (int(x), int(y)), (int(x),int(y)+int(MA/2)), (255,0,255))
+			#cv2.line(im, (int(x), int(y)), (int(x)-int(ma/2),int(y)),(255,0,255))
+		else:
+			return False
+
+		#string = "ell: {0:0.2f}, area: {0:0.2f},\n ratio: {0:0.2f}".format(ell_area, area, ratio)
+		#string = "ratio: {0:0.2f}".format(ratio)
+		#cv2.ellipse(im, ellipse, (0,0,255),2)
+		#cv2.putText(im, string, (int(x),int(y)), font, 1, (0,255,0), 1)
+		#cv2.putText(im,'OpenCV',(10,500), font, 4,(255,255,255),2)
 
 
 
-    def filter_and_mask(self, frame):
-        #blur = cv2.GaussianBlur(frame,(0,0),3)
-        cv2.imshow('filtering', frame)
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        mask = cv2.inRange(hsv, self.low_red, self.upper_red)
-        mask = cv2.erode(mask, None, iterations=2)
-        mask = cv2.dilate(mask, None, iterations=2)
-        return mask
+		if 1.4 < ratio < 1.6:
+			#cv2.putText(im, "Balloon", (int(x),int(y)), font, 1, (255,0,255), 9)
+			return True
+		else:
+			return False
 
-    def find_balloons(self):
-        """finds list of all balloon-like contours in image
-            recommended to use an image that has masked out all non-red
-        """
 
-        ret, im = self.cam.read()
+	def is_balloon(self, contour):
+		
+		sld = self.is_solid(contour)
+		rnd = self.is_round(contour)
+		ep = self.is_elliptical(contour)
+		
+		return (ep and sld and rnd)
 
-        mask = self.filter_and_mask(im)
-        
-        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-        balloons = []
-        
-        #TODO(Ahmed): figure out how to make this useful.
-        for c in cnts:
-            #peri = cv2.arcLength(c, True)
-            #approx = cv2.approxPolyDP(c, 0.02*peri, True)
-            if self.is_balloon(c):
-                #cv2.drawContours(image, [c], 0, (255,0,0), 8)
-                balloons.append(c) 
-        return balloons
+
+
+	def filter_and_mask(self, frame):
+		#blur = cv2.GaussianBlur(frame,(0,0),3)
+		cv2.imshow('filtering', frame)
+		hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+		mask = cv2.inRange(hsv, self.low_red, self.upper_red)
+		mask = cv2.erode(mask, None, iterations=2)
+		mask = cv2.dilate(mask, None, iterations=2)
+		return mask
+
+	def find_balloons(self):
+		"""finds list of all balloon-like contours in image
+			recommended to use an image that has masked out all non-red
+		"""
+
+		ret, im = self.cam.read()
+
+		mask = self.filter_and_mask(im)
+		
+		cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+		balloons = []
+		
+		#TODO(Ahmed): figure out how to make this useful.
+		for c in cnts:
+			#peri = cv2.arcLength(c, True)
+			#approx = cv2.approxPolyDP(c, 0.02*peri, True)
+			if self.is_balloon(c):
+				#cv2.drawContours(image, [c], 0, (255,0,0), 8)
+				balloons.append(c) 
+		return balloons
 
 
 
 bf = BalloonFinder()
 while True:
-    bloons = bf.find_balloons()
-    print "balloons: ", len(bloons)
-    if(len(bloons)>0):
-        contour=bloons[0]
-        (x,y), (MA,ma), angle = cv2.fitEllipse(contour)
-        ellipse = cv2.fitEllipse(contour)
-        eps = 0.1*cv2.arcLength(contour,True)
-        aprx = cv2.approxPolyDP(contour, eps, True)
-        area = cv2.contourArea(aprx)
-        radius = MA/2
-        print "radius : ", radius
-        distance=bf.get_distance_from_pixels(radius, 0.2)
-        bf.project_position(0,0,0,distance)
-        
-    k = cv2.waitKey(5) & 0xFF
-    if k ==27:
-        break
+	bloons = bf.find_balloons()
+	print "balloons: ", len(bloons)
+	if(len(bloons)>0):
+		contour=bloons[0]
+		(x,y), (MA,ma), angle = cv2.fitEllipse(contour)
+		ellipse = cv2.fitEllipse(contour)
+		eps = 0.1*cv2.arcLength(contour,True)
+		aprx = cv2.approxPolyDP(contour, eps, True)
+		area = cv2.contourArea(aprx)
+		radius = MA/2
+		print "radius : ", radius
+		distance=bf.get_distance_from_pixels(radius, 0.2)
+		bf.project_position(0,0,0,distance)
+		
+	k = cv2.waitKey(5) & 0xFF
+	if k ==27:
+		break
 
